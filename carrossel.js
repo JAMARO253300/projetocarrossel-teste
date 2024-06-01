8fe476eb0768313ca4c7fbae51bb6ad7b40cff92
JAMARO253300 <jamaro.sfilho@gmail.com>
2024-06-01 13:56:02 -0300

    alteracões no java

---
+++ carrossel.js
@@ -0,0 +1,16 @@
+let btnNext = document.querySelector(".next")
+let btnBack = document.querySelector(".back")
+
+let container = document.querySelector(".container")
+let list = document.querySelector(".container .list")
+let thumb = document.querySelector(".container .thumb")
+
+function moveItemsOnClick(type) {
+
+    
+}
+
+
+
+
+
